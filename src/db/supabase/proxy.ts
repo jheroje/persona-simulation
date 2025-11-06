9e2df113136729c9c96f259dd2c75a65df20c1c5
jheroje <javierho93@gmail.com>
2025-11-06 00:25:35 +0000

    change environment vars

--- src/db/supabase/proxy.ts
+++ src/db/supabase/proxy.ts
@@ -3,8 +3,8 @@ import type { NextRequest, NextResponse } from 'next/server';
 
 export function proxySupabase(req: NextRequest, res: NextResponse) {
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,
     {
       cookies: {
         getAll() {
