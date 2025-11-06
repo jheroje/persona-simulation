adb210ef2be14fe4f0695846e8317cf66d1b0edf
jheroje <javierho93@gmail.com>
2025-11-06 01:34:44 +0000

    log in and sign up improvements

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -30,42 +30,50 @@ export default function Login() {
 
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <div className="flex flex-col space-y-4 p-8 border rounded-lg shadow-xl w-100">
-        <h2 className="text-2xl font-bold text-center">Log In</h2>
+      <div className="flex flex-col space-y-4 p-8 border border-neutral-400 rounded-lg shadow-xl w-100 bg-neutral-900">
+        <h2 className="text-2xl font-bold text-center mb-5">
+          Log in to your account
+        </h2>
         <form onSubmit={handleLogIn} className="flex flex-col space-y-3">
+          <label htmlFor="email" className="font-semibold">
+            Email
+          </label>
           <input
+            name="email"
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            placeholder="Email"
+            placeholder="user@example.com"
             required
-            className="p-2 border rounded"
+            className="p-2 border rounded bg-neutral-800 border-neutral-400"
           />
+          <label htmlFor="password" className="font-semibold">
+            Password
+          </label>
           <input
+            name="password"
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
             required
-            className="p-2 border rounded"
+            className="p-2 border rounded bg-neutral-800 border-neutral-400"
           />
           <button
             type="submit"
-            className="bg-green-500 hover:bg-green-600 text-white p-3 rounded-md font-semibold cursor-pointer"
+            className="bg-green-500 hover:bg-green-600 disabled:bg-neutral-500 text-white p-3 rounded-md font-bold cursor-pointer disabled:cursor-default mt-5"
+            disabled={!email || !password}
           >
             Log In
           </button>
         </form>
         <div className="text-center text-sm">
-          <p>
-            New user?
-            <Link
-              href="/auth/signup"
-              className="text-blue-600 hover:underline font-medium"
-            >
-              Create an account
-            </Link>
-          </p>
+          <p>New user?</p>
+          <Link
+            href="/auth/signup"
+            className="text-blue-600 hover:underline font-medium"
+          >
+            Create an account
+          </Link>
         </div>
       </div>
     </div>
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -49,36 +49,49 @@ export default function Signup() {
 
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <div className="flex flex-col space-y-4 p-8 border rounded-lg shadow-xl w-100">
-        <h2 className="text-2xl font-bold text-center">Create Account</h2>
+      <div className="flex flex-col space-y-4 p-8 border border-neutral-400 rounded-lg shadow-xl w-100 bg-neutral-900">
+        <h2 className="text-2xl font-bold text-center mb-5">
+          Create an account
+        </h2>
         <form onSubmit={handleSignUp} className="flex flex-col space-y-3">
+          <label htmlFor="password" className="font-semibold">
+            Username
+          </label>
           <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Email"
-            required
-            className="p-2 border rounded"
-          />
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-            required
-            className="p-2 border rounded"
-          />
-          <input
+            name="username"
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
-            placeholder="Username (for profile)"
             required
-            className="p-2 border rounded"
+            className="p-2 border rounded bg-neutral-800 border-neutral-400"
+          />
+          <label htmlFor="email" className="font-semibold">
+            Email
+          </label>
+          <input
+            name="email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="user@example.com"
+            required
+            className="p-2 border rounded bg-neutral-800 border-neutral-400"
+          />
+          <label htmlFor="password" className="font-semibold">
+            Password
+          </label>
+          <input
+            name="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            className="p-2 border rounded bg-neutral-800 border-neutral-400"
           />
           <button
             type="submit"
-            className="bg-blue-500 hover:bg-blue-600 text-white p-3 rounded-md font-semibold cursor-pointer"
+            className="bg-blue-500 hover:bg-blue-600 disabled:bg-neutral-500 text-white p-3 rounded-md font-bold cursor-pointer disabled:cursor-default mt-5"
+            disabled={!username || !email || !password}
           >
             Create Account
           </button>
